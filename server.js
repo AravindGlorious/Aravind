c971fb276034572e8729c78689027b63471a0f3c
aravind <113155251+AravindGlorious@users.noreply.github.com>
2025-09-10 16:51:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ import { getVideoInfo, downloadVideo } from "./utils/ytdlp.js";
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// __dirname fix for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
